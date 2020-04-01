4c6a591d766a2163482f618d4f27219ebbaf7363
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-04-01 17:08:12 +0200

    Add files via upload

---
+++ 10701.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<char, int> pos;
+unordered_map<int, char> tree;
+string inord, preord;
+int preIndex;
+
+void buildTree(int ind, int inStrt, int inEnd)
+{
+	if (inStrt > inEnd) return;
+
+	char curr = preord[preIndex];
+	preIndex++;
+	tree[ind] = curr;
+
+	if (inStrt == inEnd) return;
+
+	buildTree(2*ind, inStrt, pos[curr] - 1);
+	buildTree(2*ind + 1, pos[curr] + 1, inEnd);
+}
+
+void PostOrder(int ind) {
+	if (tree.find(ind) == tree.end()) return;
+	PostOrder(2*ind);
+	PostOrder(2*ind+1);
+	cout << tree[ind];
+}
+
+int main() {
+	int T, N;
+	cin >> T;
+	while(T--) {
+		cin >> N >> preord >> inord;
+		pos.clear();
+		tree.clear();
+		for (int i = 0; i < (int) inord.length(); i++)
+			pos[inord[i]] = i;
+		preIndex = 0;
+		buildTree(1, 0, preord.length() - 1);
+		PostOrder(1);
+		cout << "\n";
+	}
+	return 0;
+}
