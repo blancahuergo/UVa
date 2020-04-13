fe505da96d3341663fa3751e6bc2d9cee4705f22
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-04-13 10:59:58 +0200

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,31 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String args[]) {
+		BigInteger x, low, high, mid;
+		String line;
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			line = sc.nextLine();
+			x = new BigInteger(line);
+			if (x.compareTo(BigInteger.ZERO) == 0) break;
+			low = BigInteger.ONE;
+			high = x;
+			mid = BigInteger.ONE;
+			while((low.add(BigInteger.ONE)).compareTo(high) < 0) {
+				mid = (low.add(high)).divide(BigInteger.valueOf(2));
+				if ((mid.multiply(mid)).compareTo(x) < 0)
+					low = mid;
+				else if ((mid.multiply(mid)).compareTo(x) == 0) {
+					low = mid;
+					break;
+				}
+				else
+					high = mid;
+			}
+			System.out.println((low.multiply(low)).toString());
+		}
+		sc.close();
+	}
+}
