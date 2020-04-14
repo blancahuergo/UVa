06ab6289dc791da73d28b29118dd08ace9cec681
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-04-14 09:26:48 +0200

    Update and rename Main.java to 11115.java

--- 11115.java
+++ 11115.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Main {
+public class 11115 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
