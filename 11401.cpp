4950d18b5e40ad2102387bfd357819f7f3b36a13
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-04-14 10:21:16 +0200

    Update 11401.cpp

--- 11401.cpp
+++ 11401.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <unordered_map>
 using namespace std;
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 	long long int N;
-	unordered_map<long long int, long long int> ans;
 	while(true) {
 		cin >> N;
 		if (N < 3) break;
-		if (ans.find(N) == ans.end())
-			ans[N] = (N-2)*N*(2*N-5)/24;
-		cout << ans[N] << "\n";
+		cout << (N-2)*N*(2*N-5)/24 << "\n";
 	}
 	return 0;
 }
