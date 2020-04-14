a16a0eb1f926f5288c4b588f3cd60114d91d7504
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-04-14 09:39:30 +0200

    Update and rename Main.java to 11609.java

--- 11609.java
+++ 11609.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Main {
+public class 11609 {
 	public static void main(String[] args) {
 		int T, N;
 		BigInteger ans, two, mod;
