bfbb5a655e070c89dbb3e194b45bf7d7dc9ad608
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-04-14 09:38:51 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int T, N;
+		BigInteger ans, two, mod;
+		two = BigInteger.valueOf(2);
+		mod = BigInteger.valueOf(1000000007);
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		for (int t = 1; t <= T; t++) {
+			N = sc.nextInt();
+			ans = two.modPow(BigInteger.valueOf(N-1), mod).multiply(BigInteger.valueOf(N)).mod(mod);
+			System.out.println("Case #" + t + ": " + ans.toString());
+		}
+		sc.close();
+	}
+}
