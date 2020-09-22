c6b06bc548feae29de824cf1804e31506c80023b
blancahuergo <44428598+blancahuergo@users.noreply.github.com>
2020-09-22 17:40:31 +0200

    Update 10158.cpp

--- 10158.cpp
+++ 10158.cpp
@@ -8,13 +8,8 @@ vector<int> enemies;
 int N;
 
 int root(int a) {
-	int last_a;
-	while(a != parent[a]) {
-		last_a = a;
-		a = parent[a];
-		parent[last_a] = parent[a];
-	}
-	return a;
+	if (a == parent[a]) return a;
+	return parent[a] = root(parent[a]);
 }
 
 void mergeByRank(int a, int b) {
@@ -22,11 +17,11 @@ void mergeByRank(int a, int b) {
 	r_a = root(a);
 	r_b = root(b);
 	if (r_a == r_b) return;
-	if (rnk[r_a] > rnk[r_b]) {
-		parent[r_b] = r_a;
-	} else if (rnk[r_b] > rnk[r_a]) {
-		parent[r_a] = r_b;
-	} else {
+	if (rnk[r_a] != rnk[r_b]) {
+		if (rnk[r_a] > rnk[r_b]) parent[r_b] = r_a;
+		else parent[r_a] = r_b;
+	}
+	else {
 		parent[r_a] = r_b;
 		rnk[r_b]++;
 	}
@@ -36,13 +31,8 @@ bool areFriends(int a, int b) {
 	int r_A, r_B;
 	r_A = root(a);
 	r_B = root(b);
-	if (r_A == r_B) {
-		return true;
-	}
-	if (enemies[r_A] != -1 && enemies[r_B] != -1 && (root(enemies[r_A]) == root(enemies[r_B]))) {
-		return true;
-	}
-	else return false;
+	if (r_A == r_B) return true;
+	return (enemies[r_A] != -1 && enemies[r_B] != -1 && (root(enemies[r_A]) == root(enemies[r_B])));
 }
 
 bool areEnemies(int a, int b) {
@@ -50,11 +40,9 @@ bool areEnemies(int a, int b) {
 	int r_A = root(a);
 	int r_B = root(b);
 	if (enemies[r_A] == -1 && enemies[r_B] == -1) return false;
-	if (enemies[r_A] == -1) {
-		return areFriends(r_A, enemies[r_B]);
-	} else if (enemies[r_B] == -1) {
-		return areFriends(r_B, enemies[r_A]);
-	} else return enemies[r_A] == r_B || enemies[r_B] == r_A;
+	if (enemies[r_A] == -1) return areFriends(r_A, enemies[r_B]);
+	else if (enemies[r_B] == -1) return areFriends(r_B, enemies[r_A]);
+	else return (root(enemies[r_A]) == r_B || root(enemies[r_B]) == r_A);
 }
 
 void setFriends(int a, int b) {
@@ -74,25 +62,15 @@ void setFriends(int a, int b) {
 void setEnemies(int a, int b) {
 	int r_A = root(a);
 	int r_B = root(b);
-	if (areFriends(a, b)) {
-		cout << "-1\n";
-		return;
-	}
-	if (enemies[r_A] != -1 && areEnemies(enemies[r_A], b)) {
-		cout << "-1\n";
-		return;
-	}
-	if (enemies[r_B] != -1 && areEnemies(enemies[r_B], a)) {
+	if (areFriends(a, b) || (enemies[r_A] != -1 && areEnemies(enemies[r_A], b)) || (enemies[r_B] != -1 && areEnemies(enemies[r_B], a))) {
 		cout << "-1\n";
 		return;
 	}
 	if (areEnemies(a, b)) return;
-	if (enemies[r_A] != -1) {
-		mergeByRank(enemies[r_A], b);
-	} else enemies[r_A] = r_B;
-	if (enemies[r_B] != -1) {
-		mergeByRank(enemies[r_B], a);
-	} else enemies[r_B] = r_A;
+	if (enemies[r_A] != -1) mergeByRank(enemies[r_A], b);
+	else enemies[r_A] = r_B;
+	if (enemies[r_B] != -1) mergeByRank(enemies[r_B], a);
+	else enemies[r_B] = r_A;
 }
 
 int main() {
@@ -105,17 +83,10 @@ int main() {
 	while(true) {
 		cin >> c >> a >> b;
 		if (!c) break;
-		if (c == 1) {
-			setFriends(a, b);
-		} else if (c == 3) {
-			if (areFriends(a, b)) cout << "1\n";
-			else cout << "0\n";
-		} else if (c == 2) {
-			setEnemies(a, b);
-		} else {
-			if (areEnemies(a, b)) cout << "1\n";
-			else cout << "0\n";
-		}
+		if (c == 1) setFriends(a, b);
+		else if (c == 3) cout << ((int) areFriends(a, b)) << "\n";
+		else if (c == 2) setEnemies(a, b);
+		else cout << ((int) areEnemies(a, b)) << "\n";
 	}
 	return 0;
 }
